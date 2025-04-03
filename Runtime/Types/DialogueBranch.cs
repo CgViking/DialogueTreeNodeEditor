fb825633d278cb693bb2123bb1ed59f5295adac8
CgViking <bjorn.andre.cock@gmail.com>
2025-04-03 21:13:25 +0200

    Made Branches work

--- Runtime/Types/DialogueBranch.cs
+++ Runtime/Types/DialogueBranch.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
 using DTNE.DialogueTreeNodeEditor.Runtime.Attributes;
 using DTNE.DialogueTreeNodeEditor.Runtime.ScriptableObjects;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace DTNE.DialogueTreeNodeEditor.Runtime.Types
 {
@@ -14,15 +12,41 @@ namespace DTNE.DialogueTreeNodeEditor.Runtime.Types
         public List<string> Choices => _choices;
         [ExposedProperty, Tooltip("The start dialogue\nCan be left blank")]
         public string StartDialogue;
+        
+        private bool _awaitingChoice;
 
         public override int FlowOutputCount => Choices.Count;
         public override int GetOutputPortCount()
         {
             return FlowOutputCount;
         }
-        public override string OnProcess(DialogueTreeAsset currentGraph, int choice)
+        public override string OnProcess(DialogueTreeAsset currentGraph, int choice = 0)
         {
-            return currentGraph.GetNodeFromOutput(id, choice)?.id;
+            if (!_awaitingChoice)
+            {
+                RaiseDialogueUpdated(new DialogueEventArgs(this.StartDialogue, this.Actor, this._choices));
+                _awaitingChoice = true;
+                return this.id;
+            }
+
+            // Reset state for future interactions
+            _awaitingChoice = false;
+
+            // Get the next node's ID based on the choice
+            string nextNodeId = currentGraph.GetNodeFromOutput(id, choice)?.id;
+
+            // Immediately trigger processing of the next node
+            if (!string.IsNullOrEmpty(nextNodeId))
+            {
+                DialogueGraphNode nextNode = currentGraph.GetNodeFromOutput(id,choice);
+                if (nextNode != null)
+                {
+                    // Process the next node
+                    return nextNode.OnProcess(currentGraph);
+                }
+            }
+
+            return base.OnProcess(currentGraph, choice); // Fallback;
         }
     }
 }
